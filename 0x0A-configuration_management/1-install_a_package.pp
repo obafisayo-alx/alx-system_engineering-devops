70ad70a1751044e0e3ddc18d2df73d1b37f48db8
obafisayo-alx <sekaniisjoy@gmail.com>
2024-03-28 23:20:03 +0100

    now

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,10 @@
-#!/usr/bin/pup
-# Installs a specific version of flask (2.1.0) from pip3.
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package {'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
+  unless  => '/usr/bin/flask --version | grep "Flask 2.1.0"',
+  require => Package['python3-pip'],
 }
