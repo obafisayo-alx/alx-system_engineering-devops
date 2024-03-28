d65e8cbb3ea31f37324ade734d1f08824e395889
obafisayo-alx <sekaniisjoy@gmail.com>
2024-03-28 23:29:07 +0100

    g

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
 # Install a specific version of flask (2.1.0) using puppet
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => ['/usr/bin', '/bin'],
+  unless  => '/usr/bin/flask --version | grep "Flask 2.1.0"',
+  require => Package['python3-pip'],
 }
