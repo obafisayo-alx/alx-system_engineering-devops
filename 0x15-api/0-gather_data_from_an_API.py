80bae95c1e5b5a6687e104a296c7c8baa988269e
obafisayo-alx <sekaniisjoy@gmail.com>
2024-05-24 14:24:28 +0100

    edited

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-"""gets api"""
+"""Using Api get todo list of an employee"""
 import requests
 from sys import argv
 
 
-def todo(userid):
-    """doc stringed"""
-    name = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(
-            userid)).json().get('name')
-    tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-            userid)).json()
-    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
-                 if dic.get('completed')]
-    if name and tasks:
-        print("Employee {} is done with tasks({}/{}):".format
-              (name, len(tasksDone), len(tasks)))
-        print(''.join(tasksDone), end='')
-
+def todos(user_id):
+    EMP_NAME = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(user_id)).json()["name"]
+    TASKS = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                            .format(user_id)).json()
+    TASKS_COMPLETED = ['\t {}\n'.format(res.get('title')) for res in TASKS
+                 if res.get('completed')]
+    NUMBER_OF_DONE_TASKS = (len(TASKS_COMPLETED))
+    TOTAL_NUMBER_OF_TASKS = (len(TASKS))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(EMP_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    print(''.join(TASKS_COMPLETED), end='')
 
 if __name__ == "__main__":
     if len(argv) == 2:
-        todo(int(argv[1]))
+        todos(int(argv[1]))
